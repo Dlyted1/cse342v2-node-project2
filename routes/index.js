6664975355964ba30ba3b79bb0e4aa391ef82f52
John Doe <johndoe@example.com>
2024-03-15 00:26:32 -0400

    added .gitignore and routes

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => { res.send('Hello World'); });
+
+router.get
+
+// router.use('/', require('./swagger'));
+// router.use('/contacts', require('./contacts'));
+
+module.exports = router;
