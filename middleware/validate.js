6ece8a7046b9cedc467f902d80fd28ef2d975834
John Doe <johndoe@example.com>
2024-03-20 13:52:32 -0400

    updated to add validation

--- middleware/validate.js
+++ middleware/validate.js
@@ -2,17 +2,16 @@ const validator = require('../helpers/validate');
 
 const saveHorse = (req, res, next) => {
     const validationRule = {
-
         name: 'required|string',
         breed: 'required|string',
         coloring: 'required|string',
         gender: 'required|string',
-        birthday: 'required|string',
+        birthday: 'string',
         discipline: 'string',
         vet: 'required|string'
     };
-    validator(req.body, validationRule, {}, (err, statis) => {
-        if (err) {
+    validator(req.body, validationRule, {}, (err, status) => {
+        if (!status) {
             res.status(412).send({
                 success: false,
                 message: 'Validation failed',
