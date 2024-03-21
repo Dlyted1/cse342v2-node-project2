b8f8f843a151913df5d8b664a7a7aad91752f048
John Doe <johndoe@example.com>
2024-03-21 13:48:46 -0400

    Updating endpoints

--- controllers/horses.js
+++ controllers/horses.js
@@ -1,41 +1,45 @@
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
-const getAll = (req, res) => {
-    mongodb
+const getAll = async (req, res) => {
+    const horseResult = await mongodb
         .getDb()
-        .db()
+        .db("project2")
         .collection('horses')
         .find()
-        .toArray((err, lists) => {
-            if (err) {
-                res.status(400).json({ message: err });
-            }
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(lists);
-        });
+    horseResult.toArray().then((lists) => {
+        // .toArray((err, lists) => {
+        //    if (err) {
+        //    res.status(400).json({ message: err });
+        //    }
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists);
+    });
 };
 
-const getSingle = (req, res) => {
+const getSingle = async (req, res) => {
     if (!ObjectId.isValid(req.params.id)) {
         res.status(400).json('Must use a valid horse id.');
     }
     const userId = new ObjectId(req.params.id);
-    mongodb
+    const horseResult = await mongodb
+        // mongodb
         .getDb()
-        .db()
+        .db("project2")
         .collection('horses')
         .find({ _id: userId })
-        .toArray((err, result) => {
-            if (err) {
-            }
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(lists[0]);
-        });
+
+    horseResult.toArray().then((lists) => {
+        // .toArray((err, result) => {
+        //    if (err) {
+        //    }
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists[0]);
+    });
 };
 
 const createHorse = async (req, res) => {
-    const contact = {
+    const horse = {
         name: req.body.name,
         breed: req.body.breed,
         coloring: req.body.coloring,
@@ -44,15 +48,28 @@ const createHorse = async (req, res) => {
         discipline: req.body.discipline,
         vet: req.body.vet
     };
-    const response = await mongodb.getDb().db().collection('horses').insertOne(horse);
-    if (response.acknowledged) {
-        res.status(201).json(response);
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while creating the horse profile.');
+
+    try {
+        const response = await mongodb
+            .getDb()
+            .db("project2")
+            .collection('horses')
+            .insertOne(horse);
+
+        if (response.acknowledged && response.ops && response.ops.length > 0) {
+            // Check if response.ops exists and has at least one element
+            // Respond with the created horse data
+            res.status(201).json(response.ops[0]); // ops contains the inserted document(s)
+        } else {
+            res.status(500).json('Some error occurred while creating the horse profile.');
+        }
+    } catch (error) {
+        console.error("Error creating horse:", error); // Log the error to console
+        res.status(500).json('Some error occurred while creating the horse profile.');
     }
 };
 
-const updateHorse = async (req, res) => {
+async function updateHorse(req, res) {
     if (!ObjectId.isValid(req.params.id)) {
         res.status(400).json('Must use a valid horse id to update.');
     }
@@ -69,7 +86,7 @@ const updateHorse = async (req, res) => {
     };
     const response = await mongodb
         .getDb()
-        .db()
+        .db("project2")
         .collection('horses')
         .replaceOne({ _id: userId }, horse);
     console.log(response);
@@ -85,12 +102,16 @@ const deleteHorse = async (req, res) => {
         res.status(400).json('Must use a valid horse id to delete.');
     }
     const userId = new ObjectId(req.params.id);
-    const response = await mongodb.getDb().db().collection('contacts').remove({ _id: userId }, true);
+    const response = await mongodb
+        .getDb()
+        .db("project2")
+        .collection('horses')
+        .deleteOne({ _id: userId }, true);
     console.log(response);
     if (response.deletedCount > 0) {
         res.status(204).send();
     } else {
-        res.status(500).json(response.error || 'Some error occurred while deleting the horse.');
+        res.status(500).json(response.error || 'Horse not found.');
     }
 };
 
