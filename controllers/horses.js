b85119d673f21cce0dd55ffb18c370fc74cc7571
John Doe <johndoe@example.com>
2024-03-22 15:13:04 -0400

    corrections to try, catch statements

--- controllers/horses.js
+++ controllers/horses.js
@@ -56,69 +56,70 @@ const createHorse = async (req, res) => {
             .collection('horses')
             .insertOne(horse);
 
-        if (response.acknowledged && response.ops && response.ops.length > 0) {
-            // Check if response.ops exists and has at least one element
-            // Respond with the created horse data
-            res.status(201).json(response.ops[0]); // ops contains the inserted document(s)
-        } else {
-            res.status(500).json('Some error occurred while creating the horse profile.');
-        }
+        // if (response.acknowledged && response.ops && response.ops.length > 0) {
+        // Check if response.ops exists and has at least one element
+
+        // Respond with the created horse data
+        res.status(201).json(response.ops[0]); // ops contains the inserted document(s)
+        // } else {
+        //    res.status(500).json('Some error occurred while creating the horse profile.');
+        // }
     } catch (error) {
         console.error("Error creating horse:", error); // Log the error to console
         res.status(500).json('Some error occurred while creating the horse profile.');
     }
-};
 
-async function updateHorse(req, res) {
-    if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json('Must use a valid horse id to update.');
-    }
-    const userId = new ObjectId(req.params.id);
-    // be aware of updateOne if you only want to update specific fields
-    const horse = {
-        name: req.body.name,
-        breed: req.body.breed,
-        coloring: req.body.coloring,
-        gender: req.body.gender,
-        birthday: req.body.birthday,
-        discipline: req.body.discipline,
-        vet: req.body.vet
+    async function updateHorse(req, res) {
+        if (!ObjectId.isValid(req.params.id)) {
+            res.status(400).json('Must use a valid horse id to update.');
+        }
+        const userId = new ObjectId(req.params.id);
+        // be aware of updateOne if you only want to update specific fields
+        const horse = {
+            name: req.body.name,
+            breed: req.body.breed,
+            coloring: req.body.coloring,
+            gender: req.body.gender,
+            birthday: req.body.birthday,
+            discipline: req.body.discipline,
+            vet: req.body.vet
+        };
+        const response = await mongodb
+            .getDb()
+            .db("project2")
+            .collection('horses')
+            .replaceOne({ _id: userId }, horse);
+        console.log(response);
+        if (response.modifiedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Some error occurred while updating the horse.');
+        }
     };
-    const response = await mongodb
-        .getDb()
-        .db("project2")
-        .collection('horses')
-        .replaceOne({ _id: userId }, horse);
-    console.log(response);
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while updating the horse.');
-    }
-};
 
-const deleteHorse = async (req, res) => {
-    if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json('Must use a valid horse id to delete.');
-    }
-    const userId = new ObjectId(req.params.id);
-    const response = await mongodb
-        .getDb()
-        .db("project2")
-        .collection('horses')
-        .deleteOne({ _id: userId }, true);
-    console.log(response);
-    if (response.deletedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Horse not found.');
-    }
-};
+    const deleteHorse = async (req, res) => {
+        if (!ObjectId.isValid(req.params.id)) {
+            res.status(400).json('Must use a valid horse id to delete.');
+        }
+        const userId = new ObjectId(req.params.id);
+        const response = await mongodb
+            .getDb()
+            .db("project2")
+            .collection('horses')
+            .deleteOne({ _id: userId }, true);
+        console.log(response);
+        if (response.deletedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Horse not found.');
+        }
+    };
 
-module.exports = {
-    getAll,
-    getSingle,
-    createHorse,
-    updateHorse,
-    deleteHorse
+    module.exports = {
+        getAll,
+        getSingle,
+        createHorse,
+        updateHorse,
+        deleteHorse
+    }
 };
--- controllers/owners.js
+++ controllers/owners.js
@@ -54,66 +54,66 @@ const createOwner = async (req, res) => {
             .collection('owners')
             .insertOne(owner);
 
-        if (response.acknowledged && response.ops && response.ops.length > 0) {
-            // Check if response.ops exists and has at least one element
-            // Respond with the created horse data
-            res.status(201).json(response.ops[0]); // ops contains the inserted document(s)
-        } else {
-            res.status(500).json('Some error occurred while creating the owner profile.');
-        }
+        // if (response.acknowledged && response.ops && response.ops.length > 0) {
+        // Check if response.ops exists and has at least one element
+        // Respond with the created horse data
+        res.status(201).json(response.ops[0]); // ops contains the inserted document(s)
+        // } else {
+        // res.status(500).json('Some error occurred while creating the owner profile.');
+
     } catch (error) {
         console.error("Error creating owner:", error); // Log the error to console
         res.status(500).json('Some error occurred while creating the owner profile.');
-    }
-};
-
-async function updateOwner(req, res) {
-    if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json('Must use a valid owner id to update.');
-    }
-    const userId = new ObjectId(req.params.id);
-    // be aware of updateOne if you only want to update specific fields
-    const owner = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.email.email,
-        phone: req.body.phone,
     };
-    const response = await mongodb
-        .getDb()
-        .db("project2")
-        .collection('owners')
-        .replaceOne({ _id: userId }, owner);
-    console.log(response);
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while updating the owner.');
-    }
-};
 
-const deleteOwner = async (req, res) => {
-    if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json('Must use a valid owner id to delete.');
-    }
-    const userId = new ObjectId(req.params.id);
-    const response = await mongodb
-        .getDb()
-        .db("project2")
-        .collection('owners')
-        .deleteOne({ _id: userId }, true);
-    console.log(response);
-    if (response.deletedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Owner not found.');
-    }
-};
+    async function updateOwner(req, res) {
+        if (!ObjectId.isValid(req.params.id)) {
+            res.status(400).json('Must use a valid owner id to update.');
+        }
+        const userId = new ObjectId(req.params.id);
+        // be aware of updateOne if you only want to update specific fields
+        const owner = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.email.email,
+            phone: req.body.phone,
+        };
+        const response = await mongodb
+            .getDb()
+            .db("project2")
+            .collection('owners')
+            .replaceOne({ _id: userId }, owner);
+        console.log(response);
+        if (response.modifiedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Some error occurred while updating the owner.');
+        }
+    };
 
-module.exports = {
-    getAll,
-    getSingle,
-    createOwner,
-    updateOwner,
-    deleteOwner
+    const deleteOwner = async (req, res) => {
+        if (!ObjectId.isValid(req.params.id)) {
+            res.status(400).json('Must use a valid owner id to delete.');
+        }
+        const userId = new ObjectId(req.params.id);
+        const response = await mongodb
+            .getDb()
+            .db("project2")
+            .collection('owners')
+            .deleteOne({ _id: userId }, true);
+        console.log(response);
+        if (response.deletedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Owner not found.');
+        }
+    };
+
+    module.exports = {
+        getAll,
+        getSingle,
+        createOwner,
+        updateOwner,
+        deleteOwner
+    }
 };
