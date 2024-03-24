f47ddfa6ab6d505dfdd0e699de8a22572295278a
John Doe <johndoe@example.com>
2024-03-24 01:20:32 -0400

    putting validation back in

--- routes/horses.js
+++ routes/horses.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const horsesController = require('../controllers/horses');
-// const validation = require('../middleware/validate');
+const validation = require('../middleware/validate');
 
 const { isAuthenticated } = require('../middleware/authenticate');
 
 router.get('/', horsesController.getAll);
 router.get('/:id', horsesController.getSingle);
-// router.post('/', validation.saveHorse, horsesController.createHorse);
-// router.put('/:id', validation.saveHorse, horsesController.updateHorse);
-// router.delete('/:id', horsesController.deleteHorse);
+router.post('/', validation.saveHorse, horsesController.createHorse);
+router.put('/:id', validation.saveHorse, horsesController.updateHorse);
+router.delete('/:id', horsesController.deleteHorse);
 
 router.post('/', isAuthenticated, horsesController.createHorse);
 router.put('/:id', isAuthenticated, horsesController.updateHorse);
