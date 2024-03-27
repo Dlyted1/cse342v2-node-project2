fea30defb079b84253390a7f69acd86098481c98
John Doe <johndoe@example.com>
2024-03-27 00:40:02 -0400

    logout update

--- routes/index.js
+++ routes/index.js
@@ -12,17 +12,17 @@ router.use('/owners', require('./owners'));
 
 router.get('/login', passport.authenticate('github'), (req, res) => { });
 
-router.get('/logout', function (req, res, next) {
-    req.logout(function (err) {
-        if (err) { return next(err); }
-        res.redirect('/');
-    });
+// router.get('/logout', function (req, res, next) {
+//     req.logout(function (err) {
+//         if (err) { return next(err); }
+//         res.redirect('/');
+//    });
+// });
+
+router.get('/logout', function (req, res) {
+    req.logout(); // This removes the req.user property
+    res.redirect('/'); // Redirect to the home page or any other page after logout
 });
 
-// router.get('/logout', function (req, res) {
-//    req.logout(); // This removes the req.user property
-//    res.redirect('/'); // Redirect to the home page or any other page after logout
-
-// });
 
 module.exports = router;
